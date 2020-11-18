a5364a9f1b869764e4719c5a992cd5a25b7802d1
mathi <mathi@LAPTOP-1KL8CQPM>
2020-11-18 11:13:17 +0100

    TP commencé Merge origin/master
    Conflicts:
    	src/main/java/MyClass.java

--- src/main/java/champollion/Enseignant.java
+++ src/main/java/champollion/Enseignant.java
@@ -1,9 +1,12 @@
+
+
 package champollion;
 
 public class Enseignant extends Personne {
 
     // TODO : rajouter les autres méthodes présentes dans le diagramme UML
-
+    
+    
     public Enseignant(String nom, String email) {
         super(nom, email);
     }
@@ -12,14 +15,17 @@ public class Enseignant extends Personne {
      * Calcule le nombre total d'heures prévues pour cet enseignant en "heures équivalent TD" Pour le calcul : 1 heure
      * de cours magistral vaut 1,5 h "équivalent TD" 1 heure de TD vaut 1h "équivalent TD" 1 heure de TP vaut 0,75h
      * "équivalent TD"
-     *
+     * @param volumeCM 
+     * @param volumeTP
+     * @param volumeTD
      * @return le nombre total d'heures "équivalent TD" prévues pour cet enseignant, arrondi à l'entier le plus proche
      *
      */
     public int heuresPrevues() {
         // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
-    }
+        // throw new UnsupportedOperationException("Pas encore implémenté");
+        return (int) Math.round((volumeTP*0.75 + volumeTD + volumeCM*1.5));
+        }
 
     /**
      * Calcule le nombre total d'heures prévues pour cet enseignant dans l'UE spécifiée en "heures équivalent TD" Pour
